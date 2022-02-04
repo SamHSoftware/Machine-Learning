c91014988cd745164a2893777302d0f183ed1562
Samuel Huguet <samhuguet1@gmail.com>
2022-02-04 12:52:07 +0000

    Updating the use_model RUNME files

---
+++ GAN_expand_microscopy_dataset/GAN_expand_microscopy_dataset/RUNME_use_model.py
@@ -0,0 +1,5 @@
+from GAN_module import * 
+
+# Function to use a trained generator to create our fake data. 
+# Function input arg 1: number_of_images [int] --> The number of images you wish to generate. 
+use_generator(number_of_images)
